dd0ff4cbbaf85e5e9118e5581faf114ff7a515e8
菅原康之 <yasu@yasu-Mac.local>
2017-10-16 21:51:00 +0900

    Revert "messageモデルの作成"
    This reverts commit 33a4448b771fd80e02500c90f6fcd4c062185504.

--- db/migrate/20171016123855_create_messages.rb
+++
@@ -1,9 +0,0 @@
-class CreateMessages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :messages do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :group, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
