19a14f73b87736b566bd0cdcda9308e721ae16bb
菅原康之 <yasu@yasu-Mac.local>
2017-10-20 19:55:29 +0900

    グループ作成のコントローラーを設定
    ・createメソッドおよびストロングパラメータを設定
    ・groupモデルを修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,13 @@ class GroupsController < ApplicationController
   end
 
   def create
+    if Group.create(group_params)
+      #グループの作成に成功したとき
+      redirect_to :root, notice: "グループを作成しました"
+    else
 
+      render :new
+    end
   end
 
   def edit
@@ -13,5 +19,11 @@ class GroupsController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ApplicationRecord
-  has_many :users, through: :members
   has_many :members
+  has_many :users, through: :members
 end
