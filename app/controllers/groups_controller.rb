9f85cadb0f8bc1f0d8ecf60d7355b050b5999221
菅原康之 <yasu@yasu-Mac.local>
2017-10-20 23:32:36 +0900

    グループ編集機能の追加
    ・groupコントローラーのedit、updateメソッドを編集
    ・editのビューを編集

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,11 +15,16 @@ class GroupsController < ApplicationController
   end
 
   def edit
-
+    @group = Group.find(params[:id])
   end
 
   def update
-
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to :root, notice: "グループを編集しました"
+    else
+      render :edit
+    end
   end
 
   private
