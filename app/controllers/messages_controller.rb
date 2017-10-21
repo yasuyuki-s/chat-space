a7ad2cf6595f8f155559153984cfadbce91eb6bf
菅原康之 <yasu@yasu-Mac.local>
2017-10-21 16:30:31 +0900

    テキストの投稿機能を実装
    ・messages#createのルーティング設定
    ・messagesコントローラーのindex、createメソッドを設定
    ・messagesコントローラーのストロングパラメーターを設定
    ・対応するビューを編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,27 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only:[:index, :create]
   def index
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      #メッセージの作成に成功したとき
+      redirect_to group_messages_path(@group)
+    else
+      #メッセージの作成に失敗したとき
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body,:image).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
+  def set_group
     @group = Group.find(params[:group_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root "groups#index"
   resources :users, only: [:edit, :update]
   resources :groups, only: [:create, :edit, :new, :update] do
-    resources :messages, only: :index
+    resources :messages, only: [:index, :create]
   end
 end
