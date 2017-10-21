dc9580dd68ff4a36a10dc0758f64388490f83ea0
菅原康之 <yasu@yasu-Mac.local>
2017-10-21 19:56:59 +0900

    messages#createメソッドを修正
    ・user_idの設定にアソシエーションを利用

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.new(message_params)
     if @message.save
       #メッセージの作成に成功したとき
       redirect_to group_messages_path(@group)
@@ -20,7 +20,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:body, :image, :image_cache).merge(group_id: params[:group_id], user_id: current_user.id)
+    params.require(:message).permit(:body, :image, :image_cache).merge(group_id: params[:group_id])
   end
 
   def set_group
