a77d490f49eae64ff439c087592e58344eb3be22
菅原康之 <yasu@yasu-Mac.local>
2017-11-02 17:09:57 +0900

    POST messages#createのテストを実装
    テスト項目は以下の通り
    -ログインしている場合
    —メッセージがバリデーションを通過した場合
    -—データベースにメッセージが登録されているか
    —-indexのビューが描画されているか
    —メッセージがバリデーションを通過しなかった場合
    —データベースにメッセージが登録されていないか
    —-indexのビューが描画され、alertが表示されているか
    -ログインしていない場合
    --ログイン画面にリダイレクトされているか

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 describe MessagesController do
   let(:user) { create(:user) }
   let(:group) { create(:group) }
+  let(:messages) { create_list(:message, 3, user: user, group: group) }
+  let(:message) { build(:message, user: user, group: group) }
 
   describe "GET #index" do
     context "logged in" do
@@ -12,7 +14,6 @@ describe MessagesController do
       end
 
       it "populates an array of @group_messages ordered by created_at ASC" do
-        messages = create_list(:message, 3, user: user, group: group)
         messages.sort!{|a,b| a.created_at <=> b.created_at}
         get :index, params: {group_id: group.id}
         expect(assigns(:group_messages)).to match(messages)
@@ -31,6 +32,58 @@ describe MessagesController do
 
       it "redirects to users/sign_in" do
         get :index, params: {group_id: group.id}
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe "POST #create" do
+    context "logged in" do
+      before do
+        login_user user
+        create(:member, user: user, group: group)
+      end
+
+      context "passed validation" do
+
+        it "success to save message" do
+          expect do
+            post :create, params: {message: {body: message.body, image: message.image }, group_id: group.id }
+          end.to change(Message, :count).by(1)
+        end
+
+        it "renders the :index template" do
+          post :create, params: {message: {body: message.body, image: message.image }, group_id: group.id }
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+
+      context "failed validation" do
+
+        it "should not save message" do
+          expect do
+            post :create, params: {message: {body: "", image: "" }, group_id: group.id }
+          end.to change(Message, :count).by(0)
+        end
+
+        it "renders the :index template and alert" do
+          post :create, params: {message: {body: "", image: "" }, group_id: group.id }
+          expect(response).to redirect_to(group_messages_path(group))
+          expect(flash[:alert]).to eq "メッセージを入力してください。"
+        end
+      end
+
+
+    end
+
+    context "not logged in" do
+      before do
+        create(:member, user: user, group: group)
+      end
+
+      it "redirects to users/sign_in" do
+        post :create, params: {message: {body: message.body, image: message.image }, group_id: group.id }
         expect(response).to redirect_to(new_user_session_path)
       end
     end
